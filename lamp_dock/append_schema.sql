b57a9f4938b25ced0e2e6d2983847836dbfe2ff3
masa <osushiiisensu@gmail.com>
2020-08-02 16:37:43 +0900

    新たなファイルappend_schema.sqlをlamp_dockディレクトリ内に追加し、新しい画面に必要なテーブルのSQLを追記しました。

---
+++ lamp_dock/append_schema.sql
@@ -0,0 +1,21 @@
+--購入履歴画面のテーブル(orders)--
+CREATE TABLE `orders` (
+    --注文番号--
+    `order_id` int(11) NOT NULL AUTO_INCREMENT,
+    --注文日時--
+    `order_date` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    --購入ユーザーID--
+    `user_id` int(11) NOT NULL,
+    --主キー--
+    primary key(order_id)
+)
+
+--購入明細画面のテーブル(order_details)--
+CREATE TABLE `order_details` (
+    --注文番号--
+    `order_id` int(11) NOT NULL,
+    --商品ID--
+    `item_id` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    --商品の購入数--
+    `amount` int(11) NOT NULL
+)
